0330a338c52c903052f5e5de28e6573a17819710
Paul Bekaert <2517073B@student.gla.ac.uk>
2025-12-04 20:16:19 +0100

    sliders moving correctly

--- frontend/src/larger_projects/porfolio_optimisation/PortfolioOptimisation.tsx
+++ frontend/src/larger_projects/porfolio_optimisation/PortfolioOptimisation.tsx
@@ -3,15 +3,37 @@ import AssetSelector from "./AssetSelector";
 import VariableSlider from "../../mini_projects/util/VariableSlider";
 
 const PortfolioOptimisation = () => {
-  const [assets, setAssets] = useState<string[]>();
-  const [portfolioAllocation, setPortfolioAllocation] = useState<{ ticker: string, weight: number }>(); 
+  const [assets, setAssets] = useState<string[] | undefined>(["AAPL", "STOXX 600", "S&P"]);
+  const [portfolioAllocation, setPortfolioAllocation] = useState<{[key: string]: number}>({
+    "AAPL": 10,
+    "STOXX 600": 50,
+    "S&P": 10
+  }); 
   
-  const rebalancePortfolioOnNewAsset = () => {
+  const rebalancePortfolio = async (fixedAsset?: string, fixedValue?: number) => {
+    const original = portfolioAllocation;
+    const fixed = fixedAsset ? fixedValue ?? original[fixedAsset] : 0;
 
-  }
+    let total = 0;
+    for (const asset in original) {
+      if (asset !== fixedAsset) total += original[asset];
+    }
+
+    const newDict: {[key: string]: number} = {};
+
+    for (const asset in original) {
+      if (asset === fixedAsset) {
+        newDict[asset] = fixed; 
+      } else {
+        newDict[asset] = (original[asset] / total) * (100 - fixed);
+      }
+    }
+
+    setPortfolioAllocation(newDict);
+  };
 
   useEffect(() => {
-    rebalancePortfolioOnNewAsset();
+    rebalancePortfolio();
   }, [assets])
 
   return (
@@ -26,8 +48,17 @@ const PortfolioOptimisation = () => {
       {assets?.map((asset) => 
         <div>
           <VariableSlider
+            label={asset}
+            value={portfolioAllocation[asset]}
             min={0}
             max={100}
+            setValue={(newValue: any) => {
+              setPortfolioAllocation(prev => ({
+                ...prev,
+                [asset]: newValue
+              }));
+              rebalancePortfolio(asset, newValue);
+            }}
           />
         </div>
       )}
